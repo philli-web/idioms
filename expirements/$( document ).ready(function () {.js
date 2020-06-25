d0cbcede9087c3be1f9bfae244e13213a02ddf35
philli-web <65615471+philli-web@users.noreply.github.com>
2020-06-25 08:57:08 +0300

    Add files via upload
    zx

---
+++ expirements/$( document ).ready(function () {.js
@@ -0,0 +1,58 @@
+$( document ).ready(function () {
+  function applyNewContent(node, newTextContent) {
+    var parent = node.parentNode;
+    var spanNode = document.createElement('span');
+    spanNode.innerHTML = newTextContent;
+    var children = spanNode.childNodes;
+    for (var nextSibling = node.nextSibling; children.length > 0;) {
+      var newNode = children[children.length - 1];
+      parent.insertBefore(newNode, nextSibling);
+      nextSibling = newNode;
+    }
+    parent.removeChild(node);
+  }
+
+  var attachWordsActing = function (node, exclude) {
+    //nodeType.TEXT_NODE only
+    if(node.nodeType === 3) {
+      var prevTextContent = node.nodeValue;
+      var newTextContent = prevTextContent.replace(/[a-zA-Z][a-zA-Z'-]*/g, function (textPayload) {
+        return '<span class="context-word-action">' + textPayload + '</span>';
+      });
+      if (prevTextContent != newTextContent) {
+        applyNewContent(node, newTextContent);
+      }
+    } else {
+      if(exclude[node.tagName] == undefined) {
+        var childList =  Array.prototype.slice.call(node.childNodes);
+        for (var i = 0; i < childList.length; i++) {
+          attachWordsActing(childList[i], exclude);
+        }
+      }
+    }
+  };
+
+  $(".howling").each(function (index) {
+    attachWordsActing(this, {"A": true, "BUTTON": true});
+    $(".context-word-action").click(function (index) {
+      var that = $(this);
+
+      var done = function() {
+        that.removeClass("playing");
+      };
+
+      that.addClass("playing");
+      var word = this.innerText.toLowerCase().trim();
+      var wordPath = "file:///C:/Users/jAXT/Downloads/product/index.html/" + word + ".mp3";
+      //console.log(wordPath);
+      var sound = new Howl({ src: [wordPath], html5: true});
+      sound.once('load', function(){
+        sound.once('end', done);
+        sound.play();
+      });
+      sound.once('playerror', done);
+      sound.once('loaderror', done);
+    })
+  });
+
+});
---
+++ expirements/web.js
@@ -0,0 +1,11 @@
+
+
+var utterance = new SpeechSynthesisUtterance(' A sound mind in a sound body, Перевод- Здоровый дух в здоровом теле,  Аналог в русском- В здоровом теле здоровый дух.');
+function talk () {
+        speechSynthesis.speak (
+            new SpeechSynthesisUtterance(' A sound mind in a sound body, Перевод- Здоровый дух в здоровом теле,  Аналог в русском- В здоровом теле здоровый дух.')
+        )
+    };   
+function  stop (){
+    synth.pause();
+}
